08414e551b63cc8d1a63becf0ca2044eaf4b0b27
Don <418129463@qq.com>
2018-12-16 16:54:14 +0800

    almost be used

--- to_do/forms.py
+++ to_do/forms.py
@@ -7,3 +7,4 @@ from wtforms.validators import DataRequired, Length
 class to_do_Form(FlaskForm):
 	body = TextAreaField('待办事项:', validators=[DataRequired(), Length(1,200)])
 	submit = SubmitField('新增')
+
--- to_do/views.py
+++ to_do/views.py
@@ -8,7 +8,7 @@ from models import Thing
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-	things = Thing.query.order_by(Thing.timestamp.desc()).all()
+	things = Thing.query.filter_by(status=True).order_by(Thing.timestamp.desc()).all()
 	form = to_do_Form()
 	if form.validate_on_submit():
 		body = form.body.data
@@ -18,3 +18,12 @@ def index():
 		flash('待办事项已经添加！')
 		return redirect(url_for('index'))
 	return render_template('index.html', form=form, things=things)
+
+@app.route('/update/<int:id>', methods = ['GET', 'POST'])
+def update(id):
+	thing = Thing.query.filter_by(id=id).first()
+	thing.status = False
+	db.session.commit()
+	return redirect(url_for('index'))
+
+
